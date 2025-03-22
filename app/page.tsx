61c96ddb46b901cfcdb2e7c46d54c37f264500bb
ONENAMED <109417945+ONENAMED@users.noreply.github.com>
2025-03-22 20:14:15 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "", href: "/projects" },
+  { name: "Contact ME", href: "/something" },
   { name: "", href: "/contact" },
 ];
 
